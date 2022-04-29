50acf00eb95bf65bba3e165ba24ddf39eaad8c79
jasonglenevans <56980043+jasonglenevans@users.noreply.github.com>
2022-04-28 20:39:35 -0400

    Upload From Shell Script

---
+++ upload.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Upload From Shell Script"
+git branch -M main
+git remote add origin https://github.com/jasonglenevans/AiboRecognitionVSMelney.git
+git push -u origin main
